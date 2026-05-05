ec766df8f8f18d81ccc25c958b6c0d2d3d18c153
Magnus Runesson <Magnus.Runesson@ai.se>
2026-05-05 09:19:10 +0200

    Clean up slurm script.

--- package.sh
+++ package.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
-#SBATCH --partition=debug
+#SBATCH --partition=small
 #SBATCH --job-name=package
 #SBATCH --cpus-per-task=4
 #SBATCH --output=logs/package_%A_%a.out
-#SBATCH --error=logs/package_%A_%a.out
+#SBATCH --error=logs/package_%A_%a.err
 #SBATCH --time=00:15:00
 #SBATCH --mem=4G
 #SBATCH --nodes=1
 #SBATCH --account=project_465002530
 
-export IN_DIR=/scratch/project_465002530/training/collection/baby/dclm-1.0
-export OUT_DIR=/scratch/project_465002530/users/magnrune/tmp/
+export IN_DIR=$1
+export OUT_DIR=$2
 
-uv run main.py --input_dir ${IN_DIR} --output_dir ${OUT_DIR} --workers 1 --slurm
+uv run main.py --input_dir ${IN_DIR} --output_dir ${OUT_DIR} --workers 10 --slurm
