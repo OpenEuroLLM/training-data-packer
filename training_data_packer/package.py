e2486bd421b6bf8379e768963b1248e2a89f32a6
Magnus Runesson <Magnus.Runesson@ai.se>
2026-06-16 09:20:07 +0200

    feat: Add dynamic sampler support

--- training_data_packer/package.py
+++ training_data_packer/package.py
@@ -1,18 +1,87 @@
 import argparse
+import itertools
+import os
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
 from loguru import logger
 
-from training_data_packer.mode.release import package_file
-from training_data_packer.utils.file import change_suffix, find_files
-from training_data_packer.utils.metadata import get_metadata_value, read_metadata
+from training_data_packer.processor.clean import AlignFieldNames, field_scrubber_factory
+from training_data_packer.processor.filters import FilterOnBlocklist
+from training_data_packer.processor.pii_masking import PIIMasker
+from training_data_packer.processor.propella import propella_annotate_factory
+from training_data_packer.processor.sample import sample_register
+from training_data_packer.processor.sample.sampler import sampler_factory
+from training_data_packer.utils import metrics
+from training_data_packer.utils.file import GenericJsonlReader, JsonlZstWriter, change_suffix, find_files
+from training_data_packer.utils.metadata import get_matching_part, get_metadata_value, read_metadata
 from training_data_packer.utils.slurm import get_my_slurm_tasks
 
 
-def process(
-    collection_dir: Path, workers=1, slurm: bool = False, part: str | None = None, mode: str = "release"
+def package_file(
+    src_file: Path, metadata: dict, contamination_file: Path, pii_file: Path, propella_file: Path, out_file: Path
 ) -> None:
+    tmp_out_file = out_file.parent.joinpath("." + out_file.name)
+    if out_file.exists():
+        # File is already processed. Do not process it again
+        logger.info(f"Skipping {out_file}, already exists")
+        return
+    if tmp_out_file.exists():
+        logger.info(f"Remove old temporary file {tmp_out_file}")
+        os.remove(tmp_out_file)
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
+
+    contamination_filter = None
+    block_filter = None
+    pii_masker = None
+
+    part_config, part_name = get_matching_part(metadata, src_file)
+
+    src_reader = GenericJsonlReader(src_file)
+    align_iter = AlignFieldNames(src_reader.read(), metadata)
+    scrub_iter = field_scrubber_factory(align_iter, part_config)
+
+    propella_data_iter = None
+    if propella_file.exists():
+        propella_data_iter = GenericJsonlReader(propella_file).read()
+    propella_iter = propella_annotate_factory(scrub_iter, propella_data_iter)
+
+    # After this comment are actual records removed. Processing cannot require zipping of dataset works.
+    if "id" in metadata:
+        pii_iter = AlignFieldNames(GenericJsonlReader(pii_file).read(), metadata, no_key_hierarchy=True)
+        pii_masker = PIIMasker()
+        pii_masked_iter = map(pii_masker.get_masker(pii_iter), propella_iter)
+
+        contamination_ids = [x["id"] for x in AlignFieldNames(GenericJsonlReader(contamination_file).read(), metadata)]
+        contamination_filter = FilterOnBlocklist("contamination", contamination_ids)
+        filtered = contamination_filter.filter(pii_masked_iter)
+
+        if "block" in part_config:
+            block_filter = FilterOnBlocklist("block_list", part_config["block"])
+            filtered = block_filter.filter(filtered)
+    else:
+        logger.info("No id field in metadata, skipping pii, decontamination and blocklist")
+        filtered = scrub_iter
+    if "wds+register" in metadata:
+        logger.info("WDS+register files")
+        filtered = itertools.chain.from_iterable(map(sample_register.process_record, filtered))
+    sampled = sampler_factory(filtered, metadata, src_file)
+
+    writer = JsonlZstWriter(tmp_out_file)
+    writer.write(sampled)
+    os.rename(tmp_out_file, out_file)
+    metrics_collection = metrics.collect_metrics(
+        src_reader,
+        pii_masker,
+        contamination_filter,
+        block_filter,
+        writer,
+    )
+    metrics_filename = out_file.parent.joinpath("." + out_file.name + ".metrics.json")
+    metrics.write_metrics_to_file(metrics_collection, metrics_filename)
+
+
+def process(collection_dir: Path, workers=1, slurm: bool = False, release: str | None = None) -> None:
     metadata = read_metadata(collection_dir.joinpath("metadata.yaml"))
     source_dir = collection_dir.joinpath(metadata["release"]["default"]["input"])
     contamination_dir = collection_dir.joinpath("nemo-curator")
@@ -20,7 +89,7 @@ def process(
     propella_dir = collection_dir.joinpath("propella-4b")
     output_dir = collection_dir.joinpath("release-raw")
 
-    all_files = find_files(source_dir, metadata, part)
+    all_files = find_files(source_dir, metadata, release)
     logger.info(f"Found {len(all_files)} files")
 
     if slurm:
@@ -33,16 +102,19 @@ def process(
         jobs = []
         with ProcessPoolExecutor(max_workers=workers) as executor:
             for src_file in task_files:
-                match mode:
-                    case "release":
-                        contamination_file, pii_file, propella_file, out_file = _calculate_file_paths(
-                            src_file, source_dir, contamination_dir, pii_dir, propella_dir, output_dir, metadata
-                        )
-                        job = executor.submit(
-                            package_file, src_file, metadata, contamination_file, pii_file, propella_file, out_file
-                        )
-                    case _:
-                        raise ValueError(f"Unknown mode: {mode}")
+                contamination_file, pii_file, propella_file, out_file = _calculate_file_paths(
+                    src_file, source_dir, contamination_dir, pii_dir, propella_dir, output_dir, metadata
+                )
+
+                job = executor.submit(
+                    package_file,
+                    src_file,
+                    metadata,
+                    contamination_file,
+                    pii_file,
+                    propella_file,
+                    out_file,
+                )
                 jobs.append(job)
             executor.shutdown()
             for n, job in enumerate(jobs):
@@ -51,14 +123,10 @@ def process(
     else:
         for src_file in task_files:
             logger.debug(f"Processing file {src_file}")
-            match mode:
-                case "release":
-                    contamination_file, pii_file, propella_file, out_file = _calculate_file_paths(
-                        src_file, source_dir, contamination_dir, pii_dir, propella_dir, output_dir, metadata
-                    )
-                    package_file(src_file, metadata, contamination_file, pii_file, propella_file, out_file)
-                case _:
-                    raise ValueError(f"Unknown mode: {mode}")
+            contamination_file, pii_file, propella_file, out_file = _calculate_file_paths(
+                src_file, source_dir, contamination_dir, pii_dir, propella_dir, output_dir, metadata
+            )
+            package_file(src_file, metadata, contamination_file, pii_file, propella_file, out_file)
 
 
 def _calculate_file_paths(
@@ -100,10 +168,14 @@ def main():
         help="Only process files for my slurm partition",
         action="store_true",
     )
-    parser.add_argument("-p", "--part", help="Part to process, default is all")
-    parser.add_argument("-m", "--mode", help="Mode to run packager in", default="release", choices=["release"])
+    parser.add_argument("-r", "--release", help="Release to process, default is all")
     args = parser.parse_args()
-    process(Path(args.collection_dir), workers=args.workers, slurm=args.slurm, part=args.part, mode=args.mode)
+    process(
+        Path(args.collection_dir),
+        workers=args.workers,
+        slurm=args.slurm,
+        release=args.release,
+    )
 
 
 if __name__ == "__main__":
--- training_data_packer/processor/sample/sampler.py
+++ training_data_packer/processor/sample/sampler.py
@@ -1,17 +1,66 @@
 import itertools
 import random
-from collections.abc import Iterable
+from collections.abc import Callable, Iterable
+from importlib.machinery import SourceFileLoader
 from pathlib import Path
 from typing import Any
 
 from loguru import logger
 
 from training_data_packer.processor.sample import sample_register
-from training_data_packer.utils.metadata import get_matching_part
+from training_data_packer.utils.metadata import get_matching_part, get_metadata_value
+
+
+def read_sampler_fn(filename: Path | None) -> Callable[dict[str, Any], list[dict[str, Any]]]:
+    if filename is None:
+        return lambda x: x
+    loader = SourceFileLoader("dynamicsampler", filename)
+    sampler_module = loader.load_module()
+    return sampler_module.sample
+
+
+class DynamicSampler:
+    def __init__(self, filename, name="DynamicSampler"):
+        self._sampler_fn = read_sampler_fn(filename)
+        self._name = name
+        self._metric_lines_processed = 0
+        self._metric_lines_produced = 0
+        self._metric_lines_removed = 0
+
+    def get_metrics(self):
+        return {
+            self._name: {
+                "lines_read": self._metric_lines_processed,
+                "lines_written": self._metric_lines_produced,
+                "lines_not_keept": self._metric_lines_removed,
+            }
+        }
+
+    def get_mapper(self) -> Callable[dict[str, Any], list[dict[str, Any]]]:
+
+        def mapper(record: dict[str, Any]) -> list[dict[str, Any]]:
+            self._metric_lines_processed += 1
+            multiplier = self._sampler_fn(record)  # This method probably need some metadata and counts provided
+            result = []
+            while multiplier >= 1:
+                result.append(record)
+                multiplier -= 1
+
+            if random.random() < multiplier:
+                result.append(record)
+
+            new_lines = len(result)
+            self._metric_lines_produced += new_lines
+            if new_lines == 0:
+                self._metric_lines_removed += 1
+
+            return result
+
+        return mapper
 
 
 def sampler_factory(
-    data_iterator: Iterable[dict[str, Any]], metadata: dict, src_file_name: Path
+    data_iterator: Iterable[dict[str, Any]], metadata: dict, src_file_name: Path, part_name: str
 ) -> Iterable[dict[str, Any]]:
     if "release" not in metadata:
         return data_iterator
@@ -24,6 +73,14 @@ def sampler_factory(
             return itertools.filterfalse(lambda x: random.random() > fraction, data_iterator)
         case "wds+register":
             return itertools.chain.from_iterable(map(sample_register.process_record, data_iterator))
+        case "sampler_fn":
+            filename = get_metadata_value(
+                metadata,
+                f"release.{part_name}.sample_fn_file",
+                get_metadata_value(metadata, "release.default.sample_fn_file", None),
+            )
+            sampler = DynamicSampler(filename)
+            return itertools.chain.from_iterable(map(sampler.get_mapper(), data_iterator))
         case _:
             logger.error(f"Unknown sampling rule {release['sample']} in {release_name}")
             raise ValueError(f"Unknown sampling rule {release['sample']} in {release_name}")
