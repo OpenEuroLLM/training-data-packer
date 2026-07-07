9d24da342f97fcbbe8ebe970d057d222524f773e
Magnus Runesson <Magnus.Runesson@ai.se>
2026-07-07 15:16:19 +0200

    Fix ID key check in release mode and add tests for ID-based filtering

--- tests/mode/test_release.py
+++ tests/mode/test_release.py
@@ -102,6 +102,59 @@ class TestParallelPackagePipeline(unittest.TestCase):
 
         result, metrics = parallel_package_pipeline(src_data, metadata, part_config, piis, contaminations)
         result = list(result)
+        self.assertEqual(1, len(result))
+        self.assertOnOf(expect_on_of, result)
+
+    def test_pii_with_id(self):
+        src_data = [
+            {
+                "id": 1,
+                "source_text": "Shall we play a game?",
+                "target_text": "Duam të luajmë?",
+                "src_lang": "eng_Latn",
+                "tgt_lang": "als_Latn",
+            },
+            {
+                "id": 2,
+                "source_text": "Antarctica is the coldest place on Earth.",
+                "target_text": "Antarktida është kontinent më i ftohtë në tokë.",
+                "src_lang": "eng_Latn",
+                "tgt_lang": "als_Latn",
+            },
+        ]
+        metadata = {
+            "_internal": {"parallel": True},
+            "id": "id",
+            "text": "text",
+            "parallel": {
+                "input": "source",
+                "source": {
+                    "text": "source_text",
+                    "language": "src_lang",
+                },
+                "target": {
+                    "text": "target_text",
+                    "language": "tgt_lang",
+                },
+            },
+        }
+        part_config = {}
+        piis = iter([{"id": "2"}])
+        contaminations = iter([])
+        expect_on_of = [
+            {
+                "id": "03bcfae9b977c43b0061671d7141f318df4b975e447bdb4703439e9ec615aa89",
+                "text": "English: Shall we play a game?\nTosk Albanian: Duam të luajmë?",
+            },
+            {
+                "id": "bff982083ca3c3d9bd0e5c6396db9c14a4e34a05c9cffe3b07859d2a022fa2c0",
+                "text": "Tosk Albanian: Duam të luajmë?\nEnglish: Shall we play a game?",
+            },
+        ]
+
+        result, metrics = parallel_package_pipeline(src_data, metadata, part_config, piis, contaminations)
+        result = list(result)
+        self.assertEqual(1, len(result))
         self.assertOnOf(expect_on_of, result)
 
     def test_contamination_without_id(self):
@@ -150,4 +203,57 @@ class TestParallelPackagePipeline(unittest.TestCase):
 
         result, metrics = parallel_package_pipeline(src_data, metadata, part_config, piis, contaminations)
         result = list(result)
+        self.assertEqual(1, len(result))
+        self.assertOnOf(expect_on_of, result)
+
+    def test_contamination_with_id(self):
+        src_data = [
+            {
+                "id": "1",
+                "source_text": "Antarctica is the coldest place on Earth.",
+                "target_text": "Antarktida është kontinent më i ftohtë në tokë.",
+                "src_lang": "eng_Latn",
+                "tgt_lang": "als_Latn",
+            },
+            {
+                "id": "2",
+                "source_text": "Shall we play a game?",
+                "target_text": "Duam të luajmë?",
+                "src_lang": "eng_Latn",
+                "tgt_lang": "als_Latn",
+            },
+        ]
+        metadata = {
+            "_internal": {"parallel": True},
+            "id": "id",
+            "text": "text",
+            "parallel": {
+                "input": "source",
+                "source": {
+                    "text": "source_text",
+                    "language": "src_lang",
+                },
+                "target": {
+                    "text": "target_text",
+                    "language": "tgt_lang",
+                },
+            },
+        }
+        part_config = {}
+        piis = iter([])
+        contaminations = iter([{"id": "1"}])
+        expect_on_of = [
+            {
+                "id": "03bcfae9b977c43b0061671d7141f318df4b975e447bdb4703439e9ec615aa89",
+                "text": "English: Shall we play a game?\nTosk Albanian: Duam të luajmë?",
+            },
+            {
+                "id": "bff982083ca3c3d9bd0e5c6396db9c14a4e34a05c9cffe3b07859d2a022fa2c0",
+                "text": "Tosk Albanian: Duam të luajmë?\nEnglish: Shall we play a game?",
+            },
+        ]
+
+        result, metrics = parallel_package_pipeline(src_data, metadata, part_config, piis, contaminations)
+        result = list(result)
+        self.assertEqual(1, len(result))
         self.assertOnOf(expect_on_of, result)
